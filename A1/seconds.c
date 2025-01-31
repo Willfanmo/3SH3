f681ebe248e69f5221174e4cc8e26f89b8508194
Fan Mo <113203781+Willfanmo@users.noreply.github.com>
2025-01-30 23:16:59 -0500

    Add files via upload

---
+++ A1/seconds.c
@@ -0,0 +1,103 @@
+/**
+ * hello_oldKernel.c
+ *
+ * Kernel module that communicates with /proc file system.
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+
+#define BUFFER_SIZE 128
+
+#define PROC_NAME "seconds"
+//#define MESSAGE "Hello World\n"
+
+static unsigned long jiffies_start;
+static unsigned long elapsed_seconds;
+
+/**
+ * Function prototypes
+ */
+ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+
+static struct file_operations proc_ops = {
+        .owner = THIS_MODULE,
+        .read = proc_read,
+};
+
+
+/* This function is called when the module is loaded. */
+int proc_init(void)
+{       
+        jiffies_start = jiffies;
+        proc_create(PROC_NAME, 0, NULL, &proc_ops);
+
+        printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+        //printk(KERN_INFO "jiffies_start is %d\n", jiffies_start);
+
+	return 0;
+}
+
+/* This function is called when the module is removed. */
+void proc_exit(void) {
+
+        // removes the /proc/hello entry
+        remove_proc_entry(PROC_NAME, NULL);
+
+        printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
+}
+
+/**
+ * This function is called each time the /proc/hello is read.
+ * 
+ * This function is called repeatedly until it returns 0, so
+ * there must be logic that ensures it ultimately returns 0
+ * once it has collected the data that is to go into the 
+ * corresponding /proc file.
+ *
+ * params:
+ *
+ * file:
+ * buf: buffer in user space
+ * count:
+ * pos:
+ */
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+        int rv = 0;
+        char buffer[BUFFER_SIZE];
+        static int completed = 0;
+
+        if (completed) {
+                completed = 0;
+                return 0;
+        }
+
+        completed = 1;
+        
+        elapsed_seconds = (jiffies-jiffies_start)/HZ;
+
+
+        rv = sprintf(buffer,"Elapsed seconds: %lu\n", elapsed_seconds);
+
+        // copies the contents of buffer to userspace usr_buf
+        copy_to_user(usr_buf, buffer, rv);
+
+        return rv;
+}
+
+
+/* Macros for registering module entry and exit points. */
+module_init( proc_init );
+module_exit( proc_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Seconds Module");
+MODULE_AUTHOR("SGG");
+
