74b94410479f4552d6c22cf7813a38508aceb2b2
Patrick <wangp56@.mcmaster.ca>
2025-03-08 13:44:12 -0700

    lab 2 done

---
+++ Lab3/labMMF.c
@@ -0,0 +1,37 @@
+#include <sys/mman.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <stdlib.h>
+
+
+#define INT_SIZE 4
+#define INT_COUNT 10
+#define MEMORY_SIZE INT_COUNT * INT_SIZE
+
+int intArray[MEMORY_SIZE];
+//int intArray[INT_COUNT];
+signed char *mmapfptr;
+
+
+int main(){
+
+int mmapfile_fd = open("numbers.bin", O_RDONLY);
+
+mmapfptr = mmap(0, MEMORY_SIZE, PROT_READ, MAP_PRIVATE, mmapfile_fd, 0);
+
+for(int i =0;  i < INT_COUNT; i++){
+	memcpy(intArray + i, mmapfptr + 4*i, INT_SIZE);
+}
+munmap(mmapfptr, MEMORY_SIZE);
+
+int sum = 0;
+for(int i = 0; i <INT_COUNT; i++){
+	sum += intArray[i];
+}
+
+printf("Sum of numbers = %d\n",sum);
+
+
+return 0;
+}
