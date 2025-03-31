cf8f0488e787ea7417b80096fa2a2c6259706556
Patrick <wangp56@.mcmaster.ca>
2025-03-31 14:48:49 -0600

    skeleton

---
+++ A4/assignment4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+
+int main(){
+
+int list[20];
+int sorted_list[20];
+
+
+
+
+
+return 0;
+
+}
